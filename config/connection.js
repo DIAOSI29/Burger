5427d0d6ee3486c27b42245f5785d15e5703b83d
Tian Qin <qintian29@gmail.com>
2020-02-21 22:26:04 +0800

    change

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,28 @@
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: "3306",
-  user: "root",
-  password: "tianqin29",
-  database: "burger_db"
+connection = mysql.createConnection({
+  host: "iwqrvsv8e5fz4uni.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "p2o8ajlx4c4y1waj",
+  password: "id49yesm532wu5et",
+  database: "iavglcrs4kmtfe9y"
 });
+
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+// connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "tianqin29",
+//   database: "burger_db"
+// });
+// }
 connection.connect(err => {
   if (err) {
     console.error("error connecting: " + err.stack);
+    console.log("done?");
     return;
   }
 
